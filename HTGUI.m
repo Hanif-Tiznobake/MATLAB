784830ef25ca68c6bf61ccdd2ac714d0d06e3de6
Hanif Tiznobake <hanif@tiznobake.com>
2019-01-11 16:22:42 -0800

    minor

--- HTGUI.m
+++ HTGUI.m
@@ -3,7 +3,6 @@ classdef HTGUI < HTComponent
     %   Detailed explanation goes here
     
     properties
-        tabgroup
         components
     end
     
@@ -12,28 +11,21 @@ classdef HTGUI < HTComponent
             %UNTITLED3 Construct an instance of this class
             %   Detailed explanation goes here
             obj=obj@HTComponent(state);
-            obj.host=uifigure();
-            obj.tabgroup=uitabgroup(obj.host);
-            obj.components=HTComponent.empty(obj.count,0);
+            obj.host(1,1)=uifigure();
+            obj.host(2,1)=uitabgroup(obj.host(1,1));
+            obj.components=HTComponent.empty;
             for i = 1:obj.count
                 switch lower(state{2,2}{i,1})
                     case 'httab'
-                        obj.components(i,1) = HTTab(obj.tabgroup,obj.state{2,2}{i,2});
+                        obj.components(i,1) = HTTab(obj.host(2,1),obj.state{2,2}{i,2});
                     case 'htpanel'
-                        obj.components(i,1) = HTPanel(obj.host,obj.state{2,2}{i,2});
+                        obj.components(i,1) = HTPanel(obj.host(1,1),obj.state{2,2}{i,2});
                 end
                 obj.components(i,1).SetProps(obj.state{2,2}{i,2}{1,2});
             end
-            for i =1:size(state{1,2}{1,2},1)
-                set(obj.host,obj.state{1,2}{1,2}{i,1},obj.state{1,2}{1,2}{i,2});
-            end              
+            obj.SetProps(obj.state{1,2});
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
     end
 end
 
